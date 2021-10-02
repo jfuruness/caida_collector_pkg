12037ecfc20b1db8bf792fd34304bbe700bf9a13
jfuruness <jfuruness@gmail.com>
2021-10-02 18:15:24 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     python_requires=">=3.7",
     packages=find_packages(),
     install_requires=[
-        'lib_utils>=0.2.1',
+        'lib_utils @ git+ssh://git@github.com/jfuruness/lib_utils.git',
     ],
     classifiers=[
         'Environment :: Console',
